3ad3f7bbe73e3cf80ee5d71a848da5333f7ef9c6
Sunil Kumar P <sunilkpg23@gmail.com>
2024-08-28 20:21:13 +0530

    minor navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import './Navbar.css'
+import { VscMenu } from "react-icons/vsc";
 
 export default function Navbar() {
     return (
       <div className="nav-bar">
-        <h1>Sunil</h1>
+        <h1 className='nav-header'>Sunil</h1>
+        
+        <VscMenu size={'2%'} className='nav-menu-icon' />
+
       </div>
     );
   }
